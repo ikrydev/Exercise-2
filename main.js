df5df33e8c69bc50817e98b2502af5ffa43174eb
ikrydev <ikrydev@gmail.com>
2019-02-26 11:53:21 +0700

    2 Angka dibelakang koma

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var valueSatu, valueDua, operator;
+var valueSatu, valueDua, operator, result;
 
-var calculate = function () {
-    valueSatu = parseInt(document.querySelector('#valSatu').value);
-    valueDua = parseInt(document.querySelector('#valDua').value);
-    operator = document.querySelector('#operator').value;
+var calculate = function() {
+  valueSatu = parseInt(document.querySelector("#valSatu").value);
+  valueDua = parseInt(document.querySelector("#valDua").value);
+  operator = document.querySelector("#operator").value;
 
-    if (valueSatu && valueDua) {
-        document.querySelector('.result').style.display = 'block';
+  if (valueSatu && valueDua) {
+    document.querySelector(".result").style.display = "block";
 
-        document.querySelector('.result').textContent = Math.round(eval(valueSatu + operator + valueDua));
+    result = Number(eval(valueSatu + operator + valueDua)).toFixed(2);
+    document.querySelector(".result").textContent = Number(result);
 
-        document.querySelector('#valSatu').value = '';
-        document.querySelector('#valDua').value = '';
-    }
-}
+    document.querySelector("#valSatu").value = "";
+    document.querySelector("#valDua").value = "";
+  }
+};
 
-
-document.querySelector('button').addEventListener('click', calculate);
-document.addEventListener('keypress', function (e) {
-    if (e.keyCode == 13 || e.which == 13) {
-        calculate();
-    }
-})
+document.querySelector("button").addEventListener("click", calculate);
+document.addEventListener("keypress", function(e) {
+  if (e.keyCode == 13 || e.which == 13) {
+    calculate();
+  }
+});
