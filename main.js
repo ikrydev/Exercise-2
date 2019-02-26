39e50f80cde3f84d806009e7257d3921e69ea6e0
ikrydev <ikrydev@gmail.com>
2019-02-26 09:26:19 +0700

    use eval methods

--- main.js
+++ main.js
@@ -1,38 +1,24 @@
 var valueSatu, valueDua, operator;
 
-var startCalc = function () {
-    valueSatu = document.querySelector('#valSatu').value;
-    valueDua = document.querySelector('#valDua').value;
+var calculate = function () {
+    valueSatu = parseInt(document.querySelector('#valSatu').value);
+    valueDua = parseInt(document.querySelector('#valDua').value);
     operator = document.querySelector('#operator').value;
 
     if (valueSatu && valueDua) {
         document.querySelector('.result').style.display = 'block';
 
-        document.querySelector('.result').textContent = Math.round(calculate(parseInt(valueSatu), parseInt(valueDua), operator));
+        document.querySelector('.result').textContent = Math.round(eval(valueSatu + operator + valueDua));
 
         document.querySelector('#valSatu').value = '';
         document.querySelector('#valDua').value = '';
     }
 }
 
-var calculate = function (valSatu, valDua, operator) {
 
-    if (operator === '*') {
-        return valSatu * valDua;
-    } else if (operator === '/') {
-        return valSatu / valDua;
-    } else if (operator === '+') {
-        return valSatu + valDua;
-    } else if (operator === '-') {
-        return valSatu - valDua;
-    } else if (operator === '%') {
-        return valSatu % valDua;
-    }
-}
-
-document.querySelector('button').addEventListener('click', startCalc);
+document.querySelector('button').addEventListener('click', calculate);
 document.addEventListener('keypress', function (e) {
     if (e.keyCode == 13 || e.which == 13) {
-        startCalc();
+        calculate();
     }
 })
